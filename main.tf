ffebd2e6ebda24631261cd83b24b249f6f065a15
Pheta7 <72648824+Pheta7@users.noreply.github.com>
2026-04-25 22:01:32 +1000

    fix

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ module "blog_alb" {
       port     = 80
       protocol = "HTTP"
       forward = {
-        target_group_key = aws_lb_target_group.blog.arn
+        target_group_arn = aws_lb_target_group.blog.arn
       }
     }
   }
