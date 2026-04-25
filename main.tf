b0386348670e04971fb89f2b4a2f449488334add
Pheta7 <72648824+Pheta7@users.noreply.github.com>
2026-04-25 23:35:45 +1000

    destroy

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = [var.ami_filter.name]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
 
   filter {
@@ -11,90 +11,14 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = [var.ami_filter.owner] # Bitnami
+  owners = ["979382823631"] # Bitnami
 }
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = var.environment.name
-  cidr = "${var.environment.network_prefix}.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
-  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
-
-  enable_nat_gateway = true
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
 
   tags = {
-    Terraform   = "true"
-    Environment = var.environment.name
-  }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.1"
-
-  vpc_id              = module.blog_vpc.vpc_id
-  name                = "blog"
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-
-  security_groups = [module.blog_sg.security_group_id]
-
-  listeners = {
-    blog-http = {
-      port     = 80
-      protocol = "HTTP"
-      forward = {
-        target_group_arn = aws_lb_target_group.blog.arn
-      }
-    }
-  }
-
-  tags = {
-    Environment = var.environment.name
-  }
-}
-
-resource "aws_lb_target_group" "blog" {
-  name     = "blog"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.blog_vpc.vpc_id
-}
-
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.2.0"
-
-  name = "blog"
-
-  min_size = var.min_size
-  max_size = var.max_size
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-
-  launch_template_name = "blog"
-  security_groups      = [module.blog_sg.security_group_id]
-  instance_type        = var.instance_type
-
-  image_id = data.aws_ami.app_ami.id
-
-  traffic_source_attachments = {
-    blog-alb = {
-      traffic_source_identifier = aws_lb_target_group.blog.arn
-    }
+    Name = "HelloWorld"
   }
 }
