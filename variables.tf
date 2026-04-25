6516150667f7f1121ebe4f238b52a0a906a9c666
Pheta7 <72648824+Pheta7@users.noreply.github.com>
2026-04-25 23:14:58 +1000

    string

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "ami_filter" {
   description = "Name filter and owner for AMI"
   type = object ({
     name = string
-    owner = list(string)
+    owner = string
   })
   default  = {
     name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
