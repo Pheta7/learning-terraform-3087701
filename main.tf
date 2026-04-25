13cf34de255cd8fd8124d9566c6e230aa6b1eba9
Pheta7 <72648824+Pheta7@users.noreply.github.com>
2026-04-25 22:19:44 +1000

    fix up identifier

--- main.tf
+++ main.tf
@@ -27,21 +27,21 @@ module "blog_vpc" {
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
 module "blog_sg" {
-source  = "terraform-aws-modules/security-group/aws"
-version = "5.3.1"
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
 
-vpc_id = module.blog_vpc.vpc_id
-name = "blog"
-ingress_rules = ["http-80-tcp","https-443-tcp"]
-ingress_cidr_blocks = ["0.0.0.0/0"]
-egress_rules = ["all-all"]
-egress_cidr_blocks = ["0.0.0.0/0"]
+  vpc_id              = module.blog_vpc.vpc_id
+  name                = "blog"
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 module "blog_alb" {
@@ -76,25 +76,25 @@ resource "aws_lb_target_group" "blog" {
 }
 
 module "blog_autoscaling" {
-source  = "terraform-aws-modules/autoscaling/aws"
-version = "9.2.0"
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.2.0"
 
-name = "blog"
+  name = "blog"
 
-min_size = 1
-max_size = 2
+  min_size = 1
+  max_size = 2
 
-vpc_zone_identifier = module.blog_vpc.public_subnets
+  vpc_zone_identifier = module.blog_vpc.public_subnets
 
-launch_template_name = "blog"
-security_groups    = [module.blog_sg.security_group_id]
-instance_type      = var.instance_type
+  launch_template_name = "blog"
+  security_groups      = [module.blog_sg.security_group_id]
+  instance_type        = var.instance_type
 
-image_id           = data.aws_ami.app_ami.id
+  image_id = data.aws_ami.app_ami.id
 
-traffic_source_attachments = {
-  blog-alb = {
-    traffic_source_attachments = aws_lb_target_group.blog.arn
+  traffic_source_attachments = {
+    blog-alb = {
+      traffic_source_identifier = aws_lb_target_group.blog.arn
+    }
   }
-}
 }
